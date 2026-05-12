450ffd9e74af3965ca82098fa037d2124f4a3c79
Esraa <esraacs2012@gmail.com>
2026-05-12 22:13:45 +0400

    Fix menu category filtration

--- app/src/main/java/com/me/littlelemoncapstone/Home.kt
+++ app/src/main/java/com/me/littlelemoncapstone/Home.kt
@@ -39,6 +39,7 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.toLowerCase
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -148,7 +149,7 @@ fun MyHomeScreen(navController: NavHostController,
         ) {
             categories.forEach { category ->
                 MenuCategory(category, {
-                    filteredMenuItems = menuItems.filter { it.category == category }
+                    filteredMenuItems = menuItems.filter { it.category == category.lowercase() }
                 })
             }
         }
